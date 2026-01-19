6669e6c875eff7638063d70d5890e8c65bdca865
Ansif <stark858981@gmail.com>
2026-01-19 16:02:31 +0530

    update soon page

--- app/expo/page.jsx
+++ app/expo/page.jsx
@@ -1,11 +1,52 @@
+'use client'
+import ShinyText from '@/components/ShinyText'
 import React from 'react'
 
 const page = () => {
     return (
         <>
-            <section id='autoshow' className='h-screen'>autoshow</section>
-            <section id='proshow1' className='h-screen'>proshow1</section>
-            <section id='proshow2' className='h-screen'>proshow2</section>
+            <section id='autoshow' className='h-screen bg-black flex item-center justify-center'>
+                <ShinyText
+                    text="Coming Soon..."
+                    speed={2}
+                    delay={0}
+                    color="#b5b5b5"
+                    shineColor="#ffffff"
+                    spread={120}
+                    direction="left"
+                    yoyo={false}
+                    pauseOnHover={false}
+                    disabled={false}
+                />
+            </section>
+            <section id='proshow1' className='h-screen bg-black flex item-center justify-center'>
+                <ShinyText
+                    text="Coming Soon..."
+                    speed={2}
+                    delay={0}
+                    color="#b5b5b5"
+                    shineColor="#ffffff"
+                    spread={120}
+                    direction="left"
+                    yoyo={false}
+                    pauseOnHover={false}
+                    disabled={false}
+                />
+            </section>
+            <section id='proshow2' className='h-screen bg-black flex item-center justify-center'>
+                <ShinyText
+                    text="Coming Soon..."
+                    speed={2}
+                    delay={0}
+                    color="#b5b5b5"
+                    shineColor="#ffffff"
+                    spread={120}
+                    direction="left"
+                    yoyo={false}
+                    pauseOnHover={false}
+                    disabled={false}
+                />
+            </section>
         </>
     )
 }
