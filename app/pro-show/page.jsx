7560d9f6422e5d215b146b3fa7cb85c77dc45789
Ansif <stark858981@gmail.com>
2026-01-21 15:51:44 +0530

    add pro show 1

--- app/pro-show/page.jsx
+++ app/pro-show/page.jsx
@@ -1,10 +1,19 @@
 'use client'
+import ThreeDSlider from '@/components/3D-Slider'
+import FuzzyText from '@/components/FuzzyText'
 import ImageSlices from '@/components/RevealProShow'
 import { Vortex } from '@/components/ui/vortex'
-import { pressStart2P } from '@/lib/fonts'
 import React from 'react'
 
-const page = () => {
+const sliderItems = [
+    { imageUrl: "images/proShow/zeropause.png", data: { id: 1 } },
+    { imageUrl: "images/proshow/zeropause2.jpg", data: { id: 2 } },
+    { imageUrl: "images/proShow/zeropause.png", data: { id: 3 } },
+    { imageUrl: "images/proShow/zeropause.png", data: { id: 4 } },
+    { imageUrl: "images/proshow/zeropause2.jpg", data: { id: 5 } },
+];
+
+const Page = () => {
     return (
         <>
             <div className="fixed inset-0 -z-10 bg-black pointer-events-auto">
@@ -14,22 +23,49 @@ const page = () => {
                     particleCount={500}
                     baseHue={299}
                     className="flex items-center flex-col justify-center w-full h-full"
-                >
-                </Vortex>
-            </div>
-            <section id='proshow1' className='flex items-center flex-col justify-center w-screen h-screen text-red-600'>
-                <h3 className={`${pressStart2P.className} text-3xl drop-shadow-[2px_2px_0px_#000] drop-shadow-[#180204]`}>ZeroPause</h3>
-                <ImageSlices
-                    imageUrl='images/proshow/zeropause.png'
-                    revealImageUrl='images/proshow/zeropause2.jpg'
                 />
+            </div>
+
+            <section
+                id="proshow1"
+                className="w-screen min-h-screen flex flex-col items-center justify-center gap-10 pt-24"
+            >
+                {/* Centered FuzzyText */}
+                <div className="text-center">
+                    <FuzzyText baseIntensity={0.15} hoverIntensity={0.2} enableHover>
+                        ZeroPause
+                    </FuzzyText>
+                </div>
+
+                {/* Centered ImageSlices */}
+                <div className="w-full max-w-[600px] flex justify-center">
+                    <ImageSlices
+                        imageUrl="images/proshow/zeropause.png"
+                        revealImageUrl="images/proshow/zeropause2.jpg"
+                    />
+                </div>
+
+                {/* Completely responsive 3D slider */}
+                <div className="w-full flex justify-center items-center">
+                    <ThreeDSlider
+                        items={sliderItems}
+                        speedWheel={0.03}
+                        speedDrag={-0.15}
+                        containerStyle={{ width: '100%', height: '80vh' }}
+                    />
+                </div>
             </section>
 
-            <section id='proshow2' className='h-screen text-white'>
-                proshow2
+            <section
+                id="proshow2"
+                className="flex items-center flex-col justify-center w-screen h-screen"
+            >
+                <FuzzyText baseIntensity={0.15} hoverIntensity={0.2} enableHover>
+                    जी 𝗟𝗶𝘃𝗲
+                </FuzzyText>
             </section>
         </>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
